b3365c22fbe4c53f6bb60b4669e209d1a450d4ae
vitor <loopinfinite.all@gmail.com>
2023-03-08 14:11:26 -0300

    adjust padding container page in pages mobile

--- components/entities/List.vue
+++ components/entities/List.vue
@@ -1,12 +1,30 @@
 <template>
    <b-container>
     <b-row align-v="center" align-h="center">
-      <b-col cols="12" sm="12" md="10" lg="12">
+      <b-col cols="12" sm="12" md="10" lg="12" class="col-custom">
         <b-table striped hover
           :fields="fields"
-          :items="entities"
-          >
+          :items="fake"
+          :currentPage="currentPage"
+          :perPage="4"
+        >
+        <template #cell(#)="row">
+          <div>
+            <b-icon-pencil class="mr-2" style="cursor: pointer;"/>
+            <b-icon-trash style="cursor: pointer;"/>
+          </div>
+        </template>
         </b-table>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12" sm="12" md="10" lg="12">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="6"
+          :per-page="3"
+          aria-controls="my-table"
+        ></b-pagination>
       </b-col>
     </b-row>
   </b-container>
@@ -18,6 +36,7 @@ export default {
   name: 'ListEntities',
   data() {
     return {
+      currentPage: 1,
       fields: [
           {
             key: 'id',
@@ -34,8 +53,55 @@ export default {
           {
             key: 'order',
             sortable: true,
+          },
+          {
+            key: '#'
           }
         ],
+        fake: [
+          {
+          "id": 2,
+          "name": "No Pets",
+          "active": 1,
+          "order": 0,
+        },
+        {
+          "id": 4,
+          "name": "Events (On Application)",
+          "active": 1,
+          "order": 0
+        },
+        {
+          "id": 5,
+          "name": "Late Check-out Available",
+          "active": 1,
+          "order": 0,
+        },
+        {
+          "id": 6,
+          "name": "No Smoking",
+          "active": 1,
+          "order": 0
+        },
+        {
+          "id": 7,
+          "name": "No Parties\/Events",
+          "active": 1,
+          "order": 0
+        },
+        {
+          "id": 8,
+          "name": "Age Restriction",
+          "active": 1,
+          "order": 0
+        },
+        {
+          "id": 10,
+          "name": "New House Rules vitor",
+          "active": 1,
+          "order": 0
+        }
+      ],
         empty: [{ id: null, name: null, active: null, order: null}]
     }
   },
@@ -45,7 +111,7 @@ export default {
   computed: {
     ...mapGetters({
       'entities': 'entities/getEntities'
-    })
+    }),
   },
   methods: {
     ...mapActions({
@@ -54,3 +120,14 @@ export default {
   },
 }
 </script>
+<style lang="scss" scoped>
+.pagination{
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+}
+
+.col-custom{
+  @media (max-width: 576px) { padding: 0; }
+}
+</style>
