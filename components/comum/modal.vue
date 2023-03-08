21a611cdc15db6296b0e5a3359fe216489ea789e
vitor <loopinfinite.all@gmail.com>
2023-03-08 16:57:13 -0300

    add component comum modal

---
+++ components/comum/modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-modal
+    class="modal-task"
+    :id="reference"
+    :title="title"
+  >
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+    <template #modal-footer>
+        <b-button class="mt-3" @click="$bvModal.hide(reference)">Cancel</b-button>
+        <b-button variant="primary" class="mt-3">{{ labelAction }}</b-button>
+      </template>
+  </b-modal>
+</template>
+<script>
+export default {
+  name: 'modal',
+  props: {
+    reference:{
+      type: String,
+      required: true,
+    },
+    title:{
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    labelAction() {
+      return this.reference.includes('store')
+        ? 'Register'
+        : 'To Update'
+    }
+  }
+}
+</script>
--- components/entities/List.vue
+++ components/entities/List.vue
@@ -2,22 +2,37 @@
    <b-container>
     <b-row align-v="center" align-h="center">
       <b-col cols="12" sm="12" md="10" lg="12" class="col-custom">
-        <b-table striped hover
+        <b-icon
+          v-b-modal.store-rules
+          style="cursor: pointer;"
+          class="mb-3 icon float-right"
+          icon="plus-circle"
+          font-scale="2.2"
+          @click="setReferenceModal('store-rules')"
+        />
+        <b-table
+          striped hover
           :fields="fields"
-          :items="fake"
+          :items="getItems"
           :currentPage="currentPage"
           :perPage="4"
         >
-        <template #cell(#)="row">
+        <template #cell(#)="row" v-if="fake.length">
           <div>
-            <b-icon-pencil class="mr-2" style="cursor: pointer;"/>
-            <b-icon-trash style="cursor: pointer;"/>
+            <b-icon
+              v-b-modal.edit-rules
+              style="cursor: pointer;"
+              class="mr-2"
+              icon="pencil"
+              @click="setReferenceModal('edit-rules')"
+            />
+            <b-icon icon="trash" style="cursor: pointer;"/>
           </div>
         </template>
         </b-table>
       </b-col>
     </b-row>
-    <b-row>
+    <b-row class="row-pagination">
       <b-col cols="12" sm="12" md="10" lg="12">
         <b-pagination
           v-show="entities.length || fake.length"
@@ -28,39 +43,52 @@
         ></b-pagination>
       </b-col>
     </b-row>
+    <Modal :reference="reference" :title="title">
+      <template v-slot:content>
+        <b-form-input v-model="name" placeholder="Enter your name"></b-form-input>
+        <b-form-radio v-model="active" size="md" class="pt-2">Is active?</b-form-radio>
+      </template>
+    </Modal>
   </b-container>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex'
-
+import Modal from '../comum/modal.vue'
 export default {
   name: 'ListEntities',
+  components: {
+    Modal,
+  },
   data() {
     return {
+      reference: '',
+      title: '',
+      active: false,
+      name: '',
       currentPage: 1,
       fields: [
-          {
-            key: 'id',
-            sortable: true
-          },
-          {
-            key: 'name',
-            sortable: true
-          },
-          {
-            key: 'active',
-            sortable: true,
-          },
-          {
-            key: 'order',
-            sortable: true,
-          },
-          {
-            key: '#'
-          }
-        ],
-        fake: [
-          {
+        {
+          key: 'id',
+          sortable: true
+        },
+        {
+          key: 'name',
+          sortable: true
+        },
+        {
+          key: 'active',
+          sortable: true,
+        },
+        {
+          key: 'order',
+          sortable: true,
+        },
+        {
+          key: '#'
+        }
+      ],
+      fake: [
+        {
           "id": 2,
           "name": "No Pets",
           "active": 1,
@@ -103,7 +131,7 @@ export default {
           "order": 0
         }
       ],
-        empty: [{ id: null, name: null, active: null, order: null}]
+      empty: [{ id: null, name: null, active: null, order: null}]
     }
   },
   mounted() {
@@ -113,11 +141,22 @@ export default {
     ...mapGetters({
       'entities': 'entities/getEntities'
     }),
+    getItems() {
+      return this.fake.length
+        ? this.fake
+        : this.empty
+    }
   },
   methods: {
     ...mapActions({
       'fetchEntities': 'entities/fetchEntities'
-    })
+    }),
+    setReferenceModal(refs) {
+      let prefix = 'rules'
+      this.reference = refs
+      if(refs.includes('store')) this.title = `Register ${prefix}`
+      else this.title = `Edit ${prefix}`
+    }
   },
 }
 </script>
@@ -126,9 +165,21 @@ export default {
   display: flex;
   flex-flow: row;
   justify-content: center;
+
 }
 
 .col-custom{
   @media (max-width: 576px) { padding: 0; }
+}
+
+.icon {
+  @media (max-width: 576px) {
+    margin-right: 10px;
+    font-size: 190% !important;
+  }
+
+}
+.row-pagination{
+  @media (max-width: 992px) { justify-content: center; }
 }
 </style>
